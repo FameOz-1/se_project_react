e950866d91220009720528c9cb97316648e64b41
AUSTIN ABBOTT <AUSTIN.GREGORY.ABBOTT@OUTLOOK.COM>
2025-09-11 18:36:47 -0700

    WeatherCard/ 1st Iteration

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
 
 function App() {
   return (
     <div>
       <div className="page__content">
         <Header />
+        <Main />
       </div>
     </div>
   );
